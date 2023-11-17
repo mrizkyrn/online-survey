c0be799444429ad72386b23967d1423c5aa5518b
mrizkyrn <mrizkyr2411@gmail.com>
2023-11-17 22:50:48 +0700

    add get response to root server

--- server/index.js
+++ server/index.js
@@ -27,6 +27,10 @@ app.listen(3000, () => {
    console.log('Server running on port 3000');
 });
 
+app.get('/', (req, res) => {
+   res.send('Hello World!');
+});
+
 // Routes
 app.use('/api/surveys', surveyRoutes);
 app.use('/api', questionRoutes);
