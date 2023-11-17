2361cb4fa5ab868696b4b23c795c4fb86fd4d546
mrizkyrn <mrizkyr2411@gmail.com>
2023-11-17 23:11:13 +0700

    remove cors origin

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'https://online-survey-five.vercel.app/' }));
+app.use(cors());
 
 app.listen(3000, () => {
    console.log('Server running on port 3000');
